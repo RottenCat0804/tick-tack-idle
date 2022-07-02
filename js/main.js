ed90f22f1510916dbf86327fb41de544b32f82fe
RottenCat0804 <68549678+RottenCat0804@users.noreply.github.com>
2022-07-02 21:53:20 +0900

    [add]画面読み込み時に走る関数を追加
    - 画面を読み込み時処理
      - <body>タグの中に<main>タグが追加される
      - <main>タグの中に`<p>Hello World</p>`が表示される（動作テスト用）

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 // Strict mode を有効化
 'use strict';
+
+// 画面初期化
+window.addEventListener('DOMContentLoaded', onDOMContentLoaded());
+
+/**
+ * 画面初期化
+ */
+function onDOMContentLoaded() {
+    // <main>を追加
+    let main = document.createElement("main");
+    document.body.appendChild(main);
+    
+    // テスト用に<p>Hello World</p>を表示
+    let text = document.createTextNode("Hello World");
+    let textElement = document.createElement("p");
+    textElement.appendChild(text)
+    main.appendChild(textElement);
+}
